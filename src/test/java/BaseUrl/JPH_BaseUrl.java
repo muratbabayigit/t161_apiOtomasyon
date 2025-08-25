f51b4b9df789d0fc782d185665be4561c014f22d
babayigit <babayigit@gmail.com>
2025-08-25 20:46:51 +0300

    otomasyon 2.gün

---
+++ src/test/java/BaseUrl/JPH_BaseUrl.java
@@ -0,0 +1,14 @@
+package BaseUrl;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
+
+public class JPH_BaseUrl {
+    protected RequestSpecification specJPH;
+
+    @BeforeTest
+    public void setup(){
+        specJPH= new RequestSpecBuilder()
+                .setBaseUri("https://restful-booker.herokuapp.com/booking ").build();    }
+}
