d83d24a16e3565cff10a1e2ae5d8d3f0b4ad73c9
babayigit <babayigit@gmail.com>
2025-08-26 21:06:35 +0300

    otomasyon 3.gün

---
+++ src/test/java/testDatas/JPH_datas.java
@@ -0,0 +1,25 @@
+package testDatas;
+
+import org.json.JSONObject;
+
+public class JPH_datas {
+        public static int basariliDurumKodu=200;
+
+
+
+        public static JSONObject expDataOlustur(){
+            JSONObject expData=new JSONObject();
+            expData.put("id",22);
+            expData.put("userId",3);
+            expData.put("title","dolor sint quo a velit explicabo quia nam");
+            expData.put("body","eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+
+            return expData;
+        }
+
+        public static JSONObject expDataolustur(int id,int userId,String title, String body){
+
+
+            return expData;
+        }
+}
---
+++ src/test/java/tests/P14_TestDataKullanimi.java
@@ -0,0 +1,64 @@
+package tests;
+
+import BaseUrl.JPH_BaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import testDatas.JPH_datas;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class P14_TestDataKullanimi extends JPH_BaseUrl {
+    /*
+
+     https://jsonplaceholder.typicode.com/posts/22 url’ine bir
+    GET request yolladigimizda donen response’in
+    status kodunun 200 ve
+    response body’sinin asagida verilen ile ayni oldugunu test ediniz
+      Response body : (expected data)
+    {
+        “userId”: 3,
+        “id”: 22,
+        “title”: “dolor sint quo a velit explicabo quia nam”,
+        “body”: “eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse”
+        }
+
+ */
+    @Test
+    public void TestDataKullanimi(){
+
+        specJPH.pathParams("pp1","posts","pp2","22");
+        JSONObject expBody = JPH_datas.expDataOlustur();
+        Response response=given().spec(specJPH).when().get("/{pp1}/{pp2}");
+
+        // dönen cevabın body değerleri beklenen cevap değerleri ile karşılaştırılacaksa
+        // gelen response muhakkak jSonPath haline çevrilir
+
+        JsonPath resJP= response.jsonPath();
+        response.prettyPrint();
+
+        assertEquals(response.getStatusCode(),JPH_datas.basariliDurumKodu);
+        assertEquals(resJP.getInt("id"),expBody.getInt("id"));
+        assertEquals(resJP.getInt("userId"),expBody.getInt("userId"));
+        assertEquals(resJP.getString("title"),expBody.getString("title"));
+        assertEquals(resJP.getString("body"),expBody.getString("body"));
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
