8cfdd19528a8f5d28bed5fcc5d00302e702c648e
babayigit <babayigit@gmail.com>
2025-08-28 21:44:38 +0300

    otomasyon 3.gün

--- src/test/java/tests/P21_POST_POJOKullanimi.java
+++ src/test/java/tests/P21_POST_POJOKullanimi.java
@@ -1,6 +1,18 @@
 package tests;
 
-public class P21_POST_POJOKullanimi {
+import BaseUrl.RestFullBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import pojos.Restfull_bookingdatesPojo;
+import pojos.Restfull_expPojo;
+import pojos.Restfull_reqPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class P21_POST_POJOKullanimi extends RestFullBaseUrl {
     /*
     https://restful-booker.herokuapp.com/booking url’ine
 asagidaki body’ye sahip bir POST request gonderdigimizde
@@ -34,4 +46,28 @@ Response Body // expected data
 }
 
      */
+
+    @Test
+    public void Test01(){
+
+        specJPH.pathParam("pp1","booking");
+        Restfull_bookingdatesPojo bookingdatesPojo=new Restfull_bookingdatesPojo("2025-09-01","2025-09-10");
+        Restfull_reqPojo reqPOJO=new Restfull_reqPojo("Murat","BABAYİĞİT",1000,true,bookingdatesPojo,"wi-fi");
+
+
+        Restfull_expPojo expPojo=new Restfull_expPojo(24,reqPOJO);
+
+        Response response =given().contentType(ContentType.JSON).spec(specJPH).when().body(reqPOJO).post("/{pp1}");
+
+        Restfull_expPojo resPOJO=response.as(Restfull_expPojo.class);
+
+        assertEquals(expPojo.getBooking().getFirstname(),resPOJO.getBooking().getFirstname());
+        assertEquals(expPojo.getBooking().getLastname(),resPOJO.getBooking().getLastname());
+        assertEquals(expPojo.getBooking().getTotalprice(),resPOJO.getBooking().getTotalprice());
+        assertEquals(expPojo.getBooking().isDepositpaid(),resPOJO.getBooking().isDepositpaid());
+        assertEquals(expPojo.getBooking().getBookingdates().getCheckin(),resPOJO.getBooking().getBookingdates().getCheckin());
+        assertEquals(expPojo.getBooking().getBookingdates().getCheckout(),resPOJO.getBooking().getBookingdates().getCheckout());
+
+    }
+
 }
